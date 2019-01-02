fc279e8a5c1cc0d11263fdc8f7945d1e08205491
Grzegorz Mutkowski <szrt@poczta.onet.pl>
2019-01-02 12:56:28 +0100

    Creating event controller for list

---
+++ src/nwEventoMVCa.Web/Controllers/EventsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using nwEventoMVCa.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace nwEventoMVCa.Web.Controllers
+{
+    [Route("events")]
+    public class EventsController : Controller
+    {
+        private static readonly List<Event> _events = new List<Event>
+        {
+            new Event("Laptop", "Platne", 3000),
+            new Event("Jeans", "Bezplatne", 150),
+            new Event("Hammer", "Platne", 47)
+        };
+
+        public IActionResult Index()
+        {
+            return View(_events);
+        }
+    }
+}
