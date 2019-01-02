263978d88980fa006d05c3e7841fb6c4c8af6bb4
Grzegorz Mutkowski <szrt@poczta.onet.pl>
2019-01-02 15:28:52 +0100

    implement event service

---
+++ src/nwEventoMVCa.Core/DTO/EventDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nwEventoMVCa.Core.DTO
+{
+    public class EventDto
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
---
+++ src/nwEventoMVCa.Core/Services/IEventService.cs
@@ -0,0 +1,13 @@
+﻿using nwEventoMVCa.Core.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nwEventoMVCa.Core.Services
+{
+    public interface IEventService
+    {
+        IEnumerable<EventDto> GetAll();
+        void Add(string name, string category, decimal price);
+    }
+}
