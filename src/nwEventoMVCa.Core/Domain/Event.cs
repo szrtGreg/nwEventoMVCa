b69dd798b870a6218023496e7e0510df8a01af45
Grzegorz Mutkowski <szrt@poczta.onet.pl>
2019-01-02 22:08:39 +0100

    setName, Price and Category in event class

--- src/nwEventoMVCa.Core/Domain/Event.cs
+++ src/nwEventoMVCa.Core/Domain/Event.cs
@@ -17,8 +17,39 @@ namespace nwEventoMVCa.Core.Domain
         public Event(string name, string category, decimal price)
         {
             Id = Guid.NewGuid();
+            SetName(name);
+            SetCategory(category);
+            SetPrice(price);
+        }
+
+        public void SetName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Event name is empty.", nameof(name));
+            }
+            if (name.Length > 100)
+            {
+                throw new ArgumentException($"Event name is too long: '{name.Length}' chars.", nameof(name));
+            }
             Name = name;
+        }
+
+        public void SetCategory(string category)
+        {
+            if (string.IsNullOrWhiteSpace(category))
+            {
+                throw new ArgumentException("Event category is empty.", nameof(category));
+            }
             Category = category;
+        }
+
+        public void SetPrice(decimal price)
+        {
+            if (price < 1 || price > 100000)
+            {
+                throw new ArgumentException($"Event price is invalid: {price}.", nameof(price));
+            }
             Price = price;
         }
     }
