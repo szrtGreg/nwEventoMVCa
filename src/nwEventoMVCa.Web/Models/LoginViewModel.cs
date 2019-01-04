b96730cc61fa48f86561f7cc797b81b719906aa1
Grzegorz Mutkowski <szrt@poczta.onet.pl>
2019-01-04 13:04:56 +0100

    adding login view model

---
+++ src/nwEventoMVCa.Web/Models/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace nwEventoMVCa.Web.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
