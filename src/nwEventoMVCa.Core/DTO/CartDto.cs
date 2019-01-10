0cf722140024131257f12e3fc2e8d6e757a8247e
Grzegorz Mutkowski <szrt@poczta.onet.pl>
2019-01-10 15:53:46 +0100

    Cart service

---
+++ src/nwEventoMVCa.Core/DTO/CartDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nwEventoMVCa.Core.DTO
+{
+    public class CartDto
+    {
+        public IEnumerable<CartItemDto> Items { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
---
+++ src/nwEventoMVCa.Core/Domain/OrderItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nwEventoMVCa.Core.Domain
+{
+    public class OrderItem
+    {
+        public Guid ProductId { get; }
+        public string ProductName { get; }
+        public int Quantity { get; }
+        public decimal UnitPrice { get; }
+        public decimal TotalPrice { get; }
+
+        public OrderItem(CartItem item)
+        {
+            ProductId = item.ProductId;
+            ProductName = item.ProductName;
+            Quantity = item.Quantity;
+            UnitPrice = item.UnitPrice;
+            TotalPrice = item.TotalPrice;
+        }
+    }
+}
